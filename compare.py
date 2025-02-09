cea6cc8bcd6cb544da08071cbd90d57ed5d6d30c
sunbrandon <sunabrandon@gmail.com>
2025-02-08 23:53:27 -0800

    Update program to Instagram's new json format

--- compare.py
+++ compare.py
@@ -1,18 +1,44 @@
 import json
 
-with open('followers.json') as file:
-    followers_json = json.load(file)
+with open('followers.json', 'r') as f:
+    followers_data = json.load(f)
 
-with open('following.json') as file:
-    following_json = json.load(file)
+with open('following.json', 'r') as f:
+    following_data = json.load(f)
+
+if isinstance(following_data, dict) and "relationships_following" in following_data:
+    following_list_raw = following_data["relationships_following"]
+else:
+    following_list_raw = following_data
+
+if isinstance(followers_data, dict) and "relationships_followers" in followers_data:
+    followers_list_raw = followers_data["relationships_followers"]
+else:
+    followers_list_raw = followers_data
 
 following_list = []
-for following in following_json["relationships_following"]:
-    following_list.append(following["string_list_data"][0]["value"])
+for entry in following_list_raw:
+    if isinstance(entry, dict):
+        if "string_list_data" in entry and entry["string_list_data"]:
+            username = entry["string_list_data"][0]["value"]
+            following_list.append(username)
+    elif isinstance(entry, str):
+        following_list.append(entry)
+    else:
+        print("Unexpected data type in following:", type(entry))
 
-for follower in followers_json["relationships_followers"]:
-    if follower["string_list_data"][0]["value"] in following_list:
-        following_list.remove(follower["string_list_data"][0]["value"])
+followers_list = []
+for entry in followers_list_raw:
+    if isinstance(entry, dict):
+        if "string_list_data" in entry and entry["string_list_data"]:
+            username = entry["string_list_data"][0]["value"]
+            followers_list.append(username)
+    elif isinstance(entry, str):
+        followers_list.append(entry)
+    else:
+        print("Unexpected data type in followers:", type(entry))
 
-for user in following_list:
+non_followers = [user for user in following_list if user not in followers_list]
+
+for user in non_followers:
     print(user)
